3149860b9e2e447d9d32e8ee2d89c5605fc95a15
Danilo Mussa <danilosmussa@gmail.com>
2024-06-14 15:30:48 +0200

    ADDED A LINE

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,8 +120,8 @@ else:
 
 # BigQuery query and display results
 #st.header("Results from BigQuery")
-#query = "SELECT * FROM `twitpol.twitter_account_history.history` LIMIT 100"
-#rows = client.query(query).result()
+query = "SELECT * FROM `twitpol.twitter_account_history.history` LIMIT 100"
+rows = client.query(query).result()
 df_rows = pd.DataFrame([dict(row) for row in rows])
 df_rows.columns = ["Name", "Neuteral", "Democrats", "Republicans"]
 #st.write(df_rows)
